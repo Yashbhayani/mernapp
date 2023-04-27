f99c4ca7a0bf6cf94ef8bac4e8d5b43e6b4748c2
Yash Bhayani <83011938+Yashbhayani@users.noreply.github.com>
2023-04-27 12:19:03 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -2,23 +2,8 @@ const MongoClient = require('mongodb').MongoClient;
 const ServerApiVersion = require('mongodb').ServerApiVersion;
 const mongoose = require('mongoose');
 const mongURI = "mongodb://127.0.0.1:27017/User";
-//const mongURI = "mongodb+srv://Yash:bItHPkcS8zU9OhJf@realmcluster.s2jue.mongodb.net/?retryWrites=true&w=majority";
-//const MONGOclient = new MongoClient(mongURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-/*
 const connectToMongo = () => {
-    mongoose.connect(MONGOclient, () => {
-        console.log("Connected to MongoDB  Successfully");
-    })
-}
-*/
-const connectToMongo = () => {
-    /*MONGOclient.connect()
-         .then(() => console.log("Connected to MongoDB  Successfully"))
-         .catch(err => {
-             console.error('App starting error:', err.stack);
-             process.exit(1)
-         });*/
     mongoose.connect(
         mongURI,
         {
